dad5ccb7d22e56bcbaca7aa22cd0c428e6e209ae
Constancio Maldonado <constamaldonado123@gmail.com>
2023-08-21 05:57:40 -0400

    avances

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -18,10 +18,30 @@ beforeAll(async () => {
 })
 
 test("Get -> 'URL_BASE', should return status code 200 and res.body.length === 1", async () => {
-  const res = await request(app)
-    .get(URL_BASE)
-    .set('Authorization', `Bearer ${TOKEN}`)
-
-  //nexpect(res.status).toBe(200)
+    const res = await request(app)
+      .get(URL_BASE)
+      .set('Authorization', `Bearer ${TOKEN}`)
   
-})
+    //expect(res.status).toBe(200)
+    //expect(res.body).toBeDefined()
+    //expect(res.body).toHaveLength(1)
+  
+  })
+  test("POST -> 'URL_BASE', should return status code 201 and res.body.firstName === user.firstName", async () => {
+    const user = {
+      firstName: "Erik",
+      lastName: "Salazar",
+      email: "erik@gmail.com",
+      password: "erik1234",
+      phone: "+4312"
+    }
+    const res = await request(app)
+      .post(URL_BASE)
+      .send(user)
+  
+    userId = res.body.id
+  
+    expect(res.status).toBe(201)
+    expect(res.body).toBeDefined()
+    expect(res.body.firstName).toBe(user.firstName)
+  })
